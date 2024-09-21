c620e40bc9bea2ea844bffec77ba7c5862314d96
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2024-09-21 19:35:14 +0100

    made changes to home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
+    <Link href={"/pages/login"}>Head over to login page</Link>
     </>
   )
 }
