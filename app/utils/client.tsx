bca5423098329f240e5d33f78d93359c6fce7f70
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2024-09-21 19:07:28 +0100

    configed api for dif env.

--- app/utils/client.tsx
+++ app/utils/client.tsx
@@ -5,10 +5,10 @@ import { useCallback } from "react";
 import { toast, userToken } from "./helper";
 
 const client = (token?: string) => {
-        const axiosInstance = axios.create({
-            // baseURL: import.meta.env.DEV ? "http://localhost:5000/api" : import.meta.env.VITE_API_URL,
-            baseURL: "http://localhost:5000/api",
-        })
+    const axiosInstance = axios.create({
+        // baseURL: import.meta.env.DEV ? "http://localhost:5000/api" : import.meta.env.VITE_API_URL,
+        baseURL: !process.env.NEXT_PUBLIC_ENV ? "http://localhost:5000/api" : "https://task-manager-server-uw4k.onrender.com",
+    })
 
     const authToken = token || userToken()
 
