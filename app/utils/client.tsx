aa08959aab90e7113e083b2852e68a69371155be
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2024-09-21 19:41:31 +0100

    updated live api.

--- app/utils/client.tsx
+++ app/utils/client.tsx
@@ -7,7 +7,7 @@ import { toast, userToken } from "./helper";
 const client = (token?: string) => {
     const axiosInstance = axios.create({
         // baseURL: import.meta.env.DEV ? "http://localhost:5000/api" : import.meta.env.VITE_API_URL,
-        baseURL: !process.env.NEXT_PUBLIC_ENV ? "http://localhost:5000/api" : "https://task-manager-server-uw4k.onrender.com",
+        baseURL: !process.env.NEXT_PUBLIC_ENV ? "http://localhost:5000/api" : "https://task-manager-server-uw4k.onrender.com/api",
     })
 
     const authToken = token || userToken()
